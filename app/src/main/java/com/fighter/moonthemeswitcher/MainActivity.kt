dd8b505454fbf36934397cae21ca8ed01bc66e0c
Ahmed Nasser <ahmedzaza857@gmail.com>
2023-07-29 08:37:07 +0300

    happy initial commit 🌺

---
+++ app/src/main/java/com/fighter/moonthemeswitcher/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.fighter.moonthemeswitcher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fighter.moonthemeswitcher.ui.theme.MoonThemeSwitcherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoonThemeSwitcherTheme {
+
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MoonThemeSwitcherTheme {
+    }
+}
